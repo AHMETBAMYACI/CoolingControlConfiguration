09ae38fc99c7e28324694fdb5515329e8b5a4537
Ahmet Bamyacı <ahmet.bamyaci@mergasoft.com>
2023-05-03 03:00:32 +0300

    Makine listesi alma işlemleri

--- CoolingControl/src/app/components/app-device-list/app-device-list.component.ts
+++ CoolingControl/src/app/components/app-device-list/app-device-list.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Device } from 'src/app/models';
 
 @Component({
   selector: 'app-app-device-list',
-  templateUrl: './app-device-list.component.html',
-  styleUrls: ['./app-device-list.component.scss'],
+  templateUrl: './app-device-list.component.html'
 })
 export class AppDeviceListComponent  implements OnInit {
 
-  constructor() { }
+
+  list_data:Device[] = [];
+
+
+  constructor(private router:Router) {
+
+    this.list_data = [
+      {Id:'1', Name:'CHZ 1', Status:0},
+      {Id:'2', Name:'CHZ 2', Status:1},
+      {Id:'3', Name:'CHZ 3', Status:0},
+      {Id:'4', Name:'CHZ 4', Status:-1}
+    ]
+
+   }
 
   ngOnInit() {}
 
+  ShowDetail(data:Device){
+
+    console.log("CLICK")
+    if(!data) return;
+
+    console.log("CLICK 2")
+    this.router.navigate(['/', 'detail']);
+  
+  }
 }
---
+++ CoolingControl/src/app/models/device.model.ts
@@ -0,0 +1,8 @@
+export class Device{
+
+    constructor(){ }
+
+    Id:string = "";
+    Name:string = "";
+    Status:number = -1;
+}
